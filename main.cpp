210496af8e8aee9545cc6293f709f6c5291a2b02
JJimenez71 <jordanjimenez510@gmail.com>
2023-11-22 17:51:43 -0700

    working on generation logic

--- main.cpp
+++ main.cpp
@@ -1,19 +1,57 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <string>
+#include <vector>
 
-struct Tree{
-    std::string vars;
-    std::string constants;
-    std::string axiom;
-    std::map<std::string, std::string> rules;
+struct LSystem{
+    std::vector<char> vars; // Alphabet of LSystem
+    std::vector<char> constants; // Other characters of LSystem
+    std::string axiom; // Starting point of L System
+    std::unordered_map<char, std::string> rules; // Production Rules of LSystem
 };
+
+/*
+*   This function serves to create n generations of a LindenMayer System and return that string
+*/
+void create_generation(LSystem &ls, int n){
+    std::string gen = ls.axiom; // We will use this to append for future generations
+    std::cout << "Starting Axiom: " + gen << std::endl;
+    for(int i = 0; i < n; i++){
+
+        for(int pos = 0; pos < gen.size(); pos++){
+            char var = ls.axiom[var];
+            if(ls.rules.find(var) != ls.rules.end()) {
+                // Insert our production rule
+                // TODO: FINISH INSERTION LOGIC 
+                std::string val = ls.rules[var];
+                gen.replace(pos + 1, val);
+                pos += val.size() - 1;
+            }
+        }
+        std::cout << "Gen " + i + ": " + gen << std::endl;
+    }
+    std::cout << "Final Axiom: " + gen << std::endl;
+
+}
 
 
 int main(){
-    std::map<std::string, std::string> m;
-    m["A"] = "B";
-    Tree tree = {"A B", "", "A", m};
+    // Initialize our variables
+    std::vector<char> v;
+    v.push_back('A');
+    v.push_back('B');
 
+    // Initialize our constants
+    std::vector<char> c;
+
+    // Initialize our axiom
+    std::string s = "A";
+
+    // Initialize our production rules
+    std::unordered_map<char, std::string> m;
+    m['A'] = "B";
+
+    LSystem ls = {v, c, s, m};
+    create_generation(ls, 3);
     return 0;
 }
